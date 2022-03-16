3c31d401516f419144ae661138397b4f96315e4b
Mgen <m@mgenware.com>
2022-03-17 00:09:43 +0800

    Allow adding a custom header

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,24 @@ const defaultHeader = `/* eslint-disable */
 
 `;
 
-function getTail(val: unknown, dts: boolean) {
+export interface Options {
+  // TypeScript definition mode.
+  dts?: boolean;
+  // Allows an extra string inserted at the top of output file.
+  header?: string;
+}
+
+function getTail(val: unknown, dts: boolean | undefined) {
   if (dts && Array.isArray(val)) {
     return `: ${typeof val[0]}[]`;
   }
   return ` = ${JSON.stringify(val)}`;
 }
 
-export default function convert(obj: Record<string, unknown>, dts?: boolean): string {
-  let result = defaultHeader;
+export default function convert(obj: Record<string, unknown>, opt?: Options): string {
+  let result = `${opt?.header ?? ''}${defaultHeader}`;
   for (const [key, val] of Object.entries(obj)) {
-    result += `export${dts ? ' declare' : ''} const ${key}${getTail(val, !!dts)};\n`;
+    result += `export${opt?.dts ? ' declare' : ''} const ${key}${getTail(val, opt?.dts)};\n`;
   }
   return result;
 }
